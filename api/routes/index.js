b78009fc74ac8ef784be029c0ed8db0c54971916
Sabeur Thabti <thabti.sabeur@gmail.com>
2016-10-20 00:02:53 +0100

    Adding arabic name

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,11 +1,11 @@
 import qaris from './qaris';
-import audio_files from './audio_files';
+import audioFiles from './audio_files';
 import surahs from './surahs';
 import sections from './sections';
 
 export default (app) => {
   app.use('/qaris', qaris);
-  app.use('/audio_files', audio_files);
+  app.use('/audio_files', audioFiles);
   app.use('/surahs', surahs);
   app.use('/sections', sections);
 };
