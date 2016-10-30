322cee9f18857afa9238e1372c422e4b2f9c9985
Sabeur Thabti <thabti.sabeur@gmail.com>
2016-10-30 22:21:59 +0000

    SEO ensure title changes between landing and qari page

--- src/containers/Qari/index.js
+++ src/containers/Qari/index.js
@@ -5,7 +5,7 @@ import Grid from 'react-bootstrap/lib/Grid';
 import Row from 'react-bootstrap/lib/Row';
 import Col from 'react-bootstrap/lib/Col';
 import Button from 'react-bootstrap/lib/Button';
-
+import Helmet from 'react-helmet';
 import { load, play, next, continuous} from 'redux/modules/audioplayer';
 import { load as loadFiles } from 'redux/modules/files';
 import zeroPad from 'utils/zeroPad';
@@ -45,6 +45,7 @@ class Qaris extends Component {
 
     return (
       <div>
+        <Helmet title={`Holy Quran recritation by ${qari.name}`} />
         <Grid
           fluid
           className={styles.reciterBackground}>
