8f80a3b5be986df6f643c940146218979124c120
Sabeur Thabti <thabti.sabeur@gmail.com>
2016-11-15 09:41:55 +0000

    remove link

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,13 +2,12 @@ import React from 'react';
 import Grid from 'react-bootstrap/lib/Grid';
 import Row from 'react-bootstrap/lib/Row';
 import Col from 'react-bootstrap/lib/Col';
-import Link from 'react-router/lib/Link';
 const styles = require('./style.scss');
 export default () => (
   <Grid className={styles.header} fluid>
     <Row>
       <Col md={8} mdOffset={2} className={`text-center ${styles.header__text}`}>
-        <h1 className={styles.heading}><Link to="/" className={styles.heading__link}>QuranicAudio</Link></h1>
+        <h1 className={styles.heading}>QuranicAudio</h1>
       </Col>
     </Row>
   </Grid>
