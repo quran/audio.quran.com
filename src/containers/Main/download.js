ff0e841c2a3c813aaeeef8803c2af80d87dc8dff
Sabeur Thabti <thabti.sabeur@gmail.com>
2017-01-18 23:41:24 +0000

    remove bootstrap from pages

--- src/containers/Main/download.js
+++ src/containers/Main/download.js
@@ -1,7 +1,4 @@
 import React, { PropTypes, Component } from 'react';
-import Grid from 'react-bootstrap/lib/Grid';
-import Row from 'react-bootstrap/lib/Row';
-import Col from 'react-bootstrap/lib/Col';
 import Header from '../../components/Header';
 import { connect } from 'react-redux';
 import { asyncConnect } from 'redux-connect';
@@ -24,16 +21,12 @@ class Download extends Component {
     return (
       <div>
         <Header />
-        <Grid>
-          <Row className={styles.aboutContainer}>
-            <Col md={8} mdOffset={2}>
-              {data.loaded ? <Row>
+          <div className={styles.page}>
+              {data.loaded ? <div>
                 <h1>Surat {surahs[surahId].name.simple} by {qaris[qariId].name}</h1>
-                <a className={styles.downloadLink} href={`https://download.quranicaudio.com/quran/${qaris[qariId].relativePath}${zeroPad(surahId, 3)}.mp3`}>Download</a>
-              </Row> : <h1>Not Found</h1>}
-            </Col>
-          </Row>
-        </Grid>
+                <a className={styles.downloadLink} href={`http://download.quranicaudio.com/quran/${qaris[qariId].relativePath}${zeroPad(surahId, 3)}.mp3`}>Download</a>
+              </div> : <h1>Not Found</h1>}
+          </div>
       </div>
     );
   }
