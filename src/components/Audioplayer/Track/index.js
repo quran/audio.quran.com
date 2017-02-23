0faa2fc0b15e0d73663d1c3d38b49329aa0c7966
Sabeur Thabti <thabti.sabeur@gmail.com>
2017-02-23 23:06:20 +0000

    remove onmouse enter progress height change

--- src/components/Audioplayer/Track/index.js
+++ src/components/Audioplayer/Track/index.js
@@ -7,7 +7,6 @@ export default class Track extends Component {
     progress: PropTypes.number.isRequired,
     onTrackChange: PropTypes.func,
     simple: PropTypes.bool,
-    showExpanded: PropTypes.bool,
     style: PropTypes.object,
     progressStyle: PropTypes.object
   };
@@ -24,24 +23,11 @@ export default class Track extends Component {
   }
 
   render() {
-    const { progress, simple, style, progressStyle, showExpanded} = this.props;
+    const { progress, simple, style, progressStyle} = this.props;
     const progressStyleObject = Object.assign({}, { width: `${progress}%` }, progressStyle);
 
-
-    const handleMouse = (event) => {
-      if (!showExpanded) {
-        const elClassList = event.target.closest(`.${styles.container}`).classList;
-        if ( event.type === 'mouseenter') {
-          elClassList.add(styles.activeHover);
-        }
-        elClassList.remove(styles.activeHover);
-      }
-    };
-
     return (
-      <div ref="container" className={`${styles.container} ${showExpanded ? styles.activeHover : ''}`}
-        onMouseEnter={handleMouse}
-        onMouseLeave={handleMouse}
+      <div ref="container" className={`${styles.container} ${styles.activeHover}`}
         onClick={this.handleClick} style={style}>
         <div className={`${styles.progress} ${simple ? styles.simple : ''}`} style={progressStyleObject} />
       </div>
