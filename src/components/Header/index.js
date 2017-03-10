5c92225d0f2908bb838f2ffc58e6ec33adcfdf33
Sabeur Thabti <thabti.sabeur@gmail.com>
2017-03-10 00:13:02 +0000

    add buttons and Jumbotron

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
-const styles = require('./style.scss');
+import Jumbotron from 'quran-components/lib/Jumbotron';
+import styles from './style.scss';
+
 export default () => (
-  <div className={styles.header} fluid>
-      <div className={styles.heading__parent}>
-        <h1 className={styles.heading__text}>QuranicAudio</h1>
-      </div>
-  </div>
+    <Jumbotron>
+      <div className={styles.heading}>QuranicAudio </div>
+    </Jumbotron>
 );
--- src/containers/Qari/index.js
+++ src/containers/Qari/index.js
@@ -1,7 +1,7 @@
 import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { asyncConnect } from 'redux-connect';
-// import Button from 'react-bootstrap/lib/Button';
+
 import Button from 'quran-components/lib/Button';
 
 import Helmet from 'react-helmet';
