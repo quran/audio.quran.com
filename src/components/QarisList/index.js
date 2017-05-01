563ad72b0582d3fc79ad28d5e934a67e38118437
Sabeur Thabti <thabti.sabeur@gmail.com>
2017-05-01 16:14:04 +0100

    split the haramain section

--- src/components/QarisList/index.js
+++ src/components/QarisList/index.js
@@ -1,7 +1,7 @@
 import React, { Component, PropTypes } from 'react';
 import browserHistory from 'react-router/lib/browserHistory';
 import Link from 'react-router/lib/Link';
-const styles = require('./style.scss');
+import styles from './style.scss';
 export default class QarisList extends Component {
   static propTypes = {
     data: PropTypes.object.isRequired,
@@ -17,15 +17,10 @@ export default class QarisList extends Component {
 
     if (qaris.length < 1 || filterBasedOnSection.length < 1) return false;
 
-    return (
-      <div className={styles.container}>
-        <span
-          className={`${styles.letter} ${letter === 'I' ? styles.slimLetter : ''}`}
-        >
-          {letter}
-        </span>
+    const renderListItem = listItems => {
+      return (
         <ul className={styles.list}>
-          {filterBasedOnSection
+          {listItems
             .filter(item => item.sectionId === section && item.home)
             .map((qari, index) => (
               <li
@@ -39,7 +34,37 @@ export default class QarisList extends Component {
               </li>
             ))}
         </ul>
+      );
+    };
 
+    const isHaramainSection = section === 2;
+
+    if (isHaramainSection) {
+      const Madinah = filterBasedOnSection.filter(item =>
+        item.name.indexOf('Madinah')
+      );
+      const Makkah = filterBasedOnSection.filter(item =>
+        item.name.indexOf('Makkah')
+      );
+
+      return (
+        <div className={styles.container}>
+          <h2 className={styles.sectionSplitName}>Makkah</h2>
+          {renderListItem(Makkah)}
+          <h2 className={styles.sectionSplitName}>Madinah</h2>
+          {renderListItem(Madinah)}
+        </div>
+      );
+    }
+
+    return (
+      <div className={styles.container}>
+        <span
+          className={`${styles.letter} ${letter === 'I' ? styles.slimLetter : ''}`}
+        >
+          {letter}
+        </span>
+        {renderListItem(filterBasedOnSection)}
       </div>
     );
   }
