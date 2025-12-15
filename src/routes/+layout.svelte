9aac8aa4dbfce1db92ceae1b7b196be5180eaa35
Mustafa J <mustafa.0x@gmail.com>
2025-12-15 10:56:01 -0800

    init

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script>
+	import './layout.css';
+	import favicon from '$lib/assets/favicon.svg';
+
+	let { children } = $props();
+</script>
+
+<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+{@render children()}
