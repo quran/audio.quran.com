f27c42bd8a97737abcc55ff4947014cea6aafb13
Mustafa J <mustafa.0x@gmail.com>
2025-12-15 20:51:00 -0800

    about: match prod copy and layout

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,9 +1,27 @@
-<div
-	class="relative m-0 w-full bg-white px-[15px] py-[40px] md:mx-auto md:mb-[50px] md:max-w-[1170px]"
+<header
+	class="h-[300px] bg-[#2ca4ab] bg-[url('https://quranicaudio.com/public/images/background.jpg')] bg-cover bg-center bg-no-repeat pt-[10px] pb-[10px] text-center text-white"
 >
-	<h1 class="text-[24px] font-bold">About</h1>
-	<p class="mt-[10px]">
-		QuranicAudio is your source for high quality recitations of the Quran. Stream or download all
-		the Quran recitations.
+	<h1
+		class="relative top-[115px] left-[40px] mt-[20px] inline-block font-['Montserrat-Bold'] text-[40px] leading-[1.1] font-medium md:top-[100px] md:text-[48px]"
+	>
+		QuranicAudio
+	</h1>
+</header>
+
+<div class="relative bg-white top-[10px] mx-[5%] md:top-0 md:mx-[20%]">
+	<h1 class="mt-[20px] mb-[10px] text-[36px] font-medium">About</h1>
+	<p class="m-0">
+		QuranicAudio.com is the largest collection of cd quality Quran recitations on the internet.
+		Mp3s on this site may be downloaded and used for personal use free of charge. However, you may
+		not use these files for commercial purposes as many of these files have rules and regulations
+		that prevent their sale except by the publishing companies.
+	</p>
+	<p class="m-0 mt-[10px]">
+		Files on QuranicAudio.com come from many different sources. Many of them are hand ripped from
+		cds. Others have been downloaded from various other high quality sites on the internet,
+		including HidayahOnline, DawahAcademy, <a href="http://kalamullah.com/">Kalamullah.com</a>,
+		<a href="http://sabbir.com/">Sabbir.com</a>, <a href="http://dhikrullah.com/">dhikrullah.com</a
+		>, and others. Many of the files have also been sent to us by some of our users, may Allah
+		reward them greatly.
 	</p>
 </div>
