8f51aa578e32286339013c0ac4d197e15a58c3de
Mustafa J <mustafa.0x@gmail.com>
2025-12-16 21:52:48 -0800

    routes: remove api proxy; fix 404

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,14 +1,28 @@
 <script>
 	import { page } from '$app/state'
+	import { resolve } from '$app/paths'
 </script>
 
-<div
-	class="relative m-0 w-full bg-white px-[15px] py-[40px] md:mx-auto md:mb-[50px] md:max-w-[1170px]"
+<header
+	class="h-[300px] bg-[#2ca4ab] bg-[url('https://quranicaudio.com/public/images/background.jpg')] bg-cover bg-center bg-no-repeat pt-[115px] pb-[10px] text-center text-white md:pt-[100px]"
 >
+	<h1
+		class="mt-[20px] mb-[10px] inline-block font-['Montserrat-Bold'] text-[40px] leading-[1.1] md:text-[48px]"
+	>
+		QuranicAudio
+	</h1>
+</header>
+
+<div class="relative m-0 w-full bg-white px-[15px] pb-[35px] md:mx-auto md:mb-[50px] md:max-w-[1170px]">
 	{#if page.status === 404}
-		<h1>Doh! 404!</h1>
-		<p>These are <em>not</em> the droids you are looking for!</p>
+		<h1 class="mt-[20px] mb-[10px] text-[36px] leading-[39.6px] text-[#444]">Page not found</h1>
+		<p class="m-0 mb-[10px] text-[14px] leading-[20px] text-[#444]">
+			Sorry, we couldn’t find that page.
+		</p>
+		<a class="text-[#2ca4ab]" href={resolve('/')}>Go back home</a>
 	{:else}
-		<h1>Error {page.status}</h1>
+		<h1 class="mt-[20px] mb-[10px] text-[36px] leading-[39.6px] text-[#444]">Something went wrong</h1>
+		<p class="m-0 mb-[10px] text-[14px] leading-[20px] text-[#444]">Please try again.</p>
+		<a class="text-[#2ca4ab]" href={resolve('/')}>Go back home</a>
 	{/if}
 </div>
